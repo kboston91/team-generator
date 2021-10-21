d3d08236cced2e41e000f05eb9101677359efe2b
kboston91 <kourtneygboston@gmail.com>
2021-10-21 08:35:28 -0500

    added logic to display card info, changed the array variable and removed its passing from the generate team and write file functions

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const fs =require('fs');
-var inquirer = require('inquirer');
-const generateHTML = require('./src/generateHTML');
-const path = require('path');
-const Employee= require("./lib/employee")
-const Engineer = require("./lib/engineer")
-const Manager = require("./lib/manager")
-const Intern = require("./lib/intern")
+const fs = require("fs");
+var inquirer = require("inquirer");
+const generateHTML = require("./src/generateHTML");
+const path = require("path");
+const Employee = require("./lib/employee");
+const Engineer = require("./lib/engineer");
+const Manager = require("./lib/manager");
+const Intern = require("./lib/intern");
 
 let teamArray = [];
-
 
 // let bob = new Employee("Bob", 123, 'bob@bob.com');
 // let steve = new Engineer('engineersteve', 'steve', 456, 'steve@steve.com');
@@ -25,57 +24,57 @@ let teamArray = [];
 // console.log(y);
 // console.log(z);
 
-function newEmployee () {
+function newEmployee() {
   const memberRole = [
     {
-      type: 'list',
-      name: 'title',
-      message: 'Do you want to add more team members?',
-      choices: ['Intern', 'Engineer', 'None']
-    }
-  ]
-   inquirer 
-   .prompt (memberRole)
-   .then (roleResponse => {
-    if (roleResponse.title === 'Intern'){
+      type: "list",
+      name: "title",
+      message: "Do you want to add more team members?",
+      choices: ["Intern", "Engineer", "None"],
+    },
+  ];
+  inquirer.prompt(memberRole).then((roleResponse) => {
+    if (roleResponse.title === "Intern") {
       generateIntern();
-    } else if (roleResponse.title === 'Engineer'){
+    } else if (roleResponse.title === "Engineer") {
       generateEngineer();
-    } else if (roleResponse.title === 'None'){
+    } else if (roleResponse.title === "None") {
       generateTeam();
     }
-   })
-  
-  };
+  });
+}
 
 const managerQuestions = [
   {
-    type: 'input',
-    name: 'name',
-    message: "What is your name?"
+    type: "input",
+    name: "name",
+    message: "What is your name?",
   },
   {
-    type: 'input',
-    name: 'id',
-    message: "What is your employee id?"
+    type: "input",
+    name: "id",
+    message: "What is your employee id?",
   },
   {
-    type: 'input',
-    name: 'email',
-    message: "What is your email address?"
+    type: "input",
+    name: "email",
+    message: "What is your email address?",
   },
   {
-    type: 'input',
-    name: 'office',
-    message: "Please provide you office number."
+    type: "input",
+    name: "office",
+    message: "Please provide you office number.",
   },
-] 
+];
 
-function generateManager () {
-  inquirer
-  .prompt(managerQuestions)
-  .then((managerResponse) => {
-    let manager = new Manager (managerResponse.name, managerResponse.id, managerResponse.email, managerResponse.officeNumber);
+function generateManager() {
+  inquirer.prompt(managerQuestions).then((managerResponse) => {
+    let manager = new Manager(
+      managerResponse.name,
+      managerResponse.id,
+      managerResponse.email,
+      managerResponse.office
+    );
     teamArray.push(manager);
     newEmployee();
   });
@@ -83,32 +82,35 @@ function generateManager () {
 
 const engineerQuestions = [
   {
-    type: 'input',
-    name: 'name',
-    message: "What is your Engineer's name?"
+    type: "input",
+    name: "name",
+    message: "What is your Engineer's name?",
   },
   {
-    type: 'input',
-    name: 'id',
-    message: "What is your Engineer's employee id?"
+    type: "input",
+    name: "id",
+    message: "What is your Engineer's employee id?",
   },
   {
-    type: 'input',
-    name: 'email',
-    message: "What is your Engineer's email address?"
+    type: "input",
+    name: "email",
+    message: "What is your Engineer's email address?",
   },
   {
-    type: 'input',
-    name: 'github',
-    message: "Please provide the Engineer's github username."
-  }
-]
+    type: "input",
+    name: "github",
+    message: "Please provide the Engineer's github username.",
+  },
+];
 
-function generateEngineer () {
-  inquirer
-  .prompt(engineerQuestions)
-  .then((engineerResponse) => {
-    let engineer = new Engineer (engineerResponse.name, engineerResponse.id, engineerResponse.email, engineerResponse.github);
+function generateEngineer() {
+  inquirer.prompt(engineerQuestions).then((engineerResponse) => {
+    let engineer = new Engineer(
+      engineerResponse.name,
+      engineerResponse.id,
+      engineerResponse.email,
+      engineerResponse.github
+    );
     teamArray.push(engineer);
     newEmployee();
   });
@@ -116,60 +118,56 @@ function generateEngineer () {
 
 const internQuestions = [
   {
-    type: 'input',
-    name: 'name',
-    message: "What is your intern's name?"
+    type: "input",
+    name: "name",
+    message: "What is your intern's name?",
   },
   {
-    type: 'input',
-    name: 'id',
-    message: "What is your intern's employee id?"
+    type: "input",
+    name: "id",
+    message: "What is your intern's employee id?",
   },
   {
-    type: 'input',
-    name: 'email',
-    message: "What is your intern's email address?"
+    type: "input",
+    name: "email",
+    message: "What is your intern's email address?",
   },
   {
-    type: 'input',
-    name: 'school',
-    message: 'Where does this intern go to school?'
-  }
-]
+    type: "input",
+    name: "school",
+    message: "Where does this intern go to school?",
+  },
+];
 
-function generateIntern () {
-  inquirer
-  .prompt(internQuestions)
-  .then((internResponse) => {
-    let intern = new Intern (internResponse.name, internResponse.id, internResponse.email, internResponse.school);
+function generateIntern() {
+  inquirer.prompt(internQuestions).then((internResponse) => {
+    let intern = new Intern(
+      internResponse.name,
+      internResponse.id,
+      internResponse.email,
+      internResponse.school
+    );
     teamArray.push(intern);
     newEmployee();
   });
 }
 
-
-
-
-function writeToFile(teamArray) {
-  let htmlPath = path.join(__dirname, '/dist', 'index.html')
-  return fs.writeFile(htmlPath, generateHTML(teamArray), (err) => { 
-    if (err) { 
-      console.log(err); 
-    } 
+function writeToFile() {
+  console.log(teamArray);
+  let htmlPath = path.join(__dirname, "/dist", "index.html");
+  return fs.writeFile(htmlPath, generateHTML(teamArray), (err) => {
+    if (err) {
+      console.log(err);
+    }
   });
 }
-  
 
-function generateTeam(teamArray){
-  
-writeToFile(teamArray);
+function generateTeam() {
+  writeToFile();
 }
 
-  
+function init() {
+  generateManager();
+}
 
-  function init() {
-    generateManager();
-  }
-
-  init();
-
+init();
--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,8 +1,20 @@
-const generateHTML = (teamArray) =>{
+const generateHTML = (tArray) =>{
+  let engineerHtml = '';
+  let internHtml = '';
+
+  console.log(tArray);
+  console.log((tArray[0].constructor.name));
+  for(let i=1; i<tArray.length; i++){
+    if(tArray[i].constructor.name == 'Engineer'){
+      engineerHtml += `<div class= "card"> ${tArray[i].constructor.name} ${tArray[i].name} ${tArray[i].id} </div>`
+    } else {
+      internHtml += `<div class= "card"> ${tArray[i].constructor.name} ${tArray[i].name} ${tArray[i].id} </div>`
+    }
+  };
   return `
-  <div>manager card </div>
-  <div>engineer card </div>
-  <div>intern card </div>
+  <div class= "card">${tArray[i].constructor.name} ${tArray[0].name} ${tArray[0].id} </div>
+  ${engineerHtml}
+  ${internHtml}
   `
 }
 
@@ -13,6 +25,7 @@ module.exports = teamArray => {
     <head>
         <meta charset="UTF-8" />
         <title>Team Generator</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     </head>
 
     <body>
@@ -22,9 +35,11 @@ module.exports = teamArray => {
         <h1>My Team</h1>
         </div>
       </div>
-  ${generateHTML(teamArray)}
-    
+      <div class="row">
+      ${generateHTML(teamArray)}
+      </div>    
     </body>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
     </html>
   `
